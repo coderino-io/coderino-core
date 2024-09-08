e6afb9bb1e03214da89d54c25926e9b604313736
Marco Rehmer <mrehmer-services@mailbox.org>
2024-09-08 09:39:53 +0000

    remove config file

--- providers/aws-provider.go
+++ providers/aws-provider.go
@@ -12,8 +12,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/aws/aws-sdk-go-v2/service/iam"
-	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 func CreateEC2Instance(name string) {
@@ -30,12 +28,6 @@ func CreateEC2Instance(name string) {
 		return
 	}
 
-	// instanceProfileName, err := createIAMRoleForEC2(cfg)
-	// if err != nil {
-	// 	log.Fatalf("Unable to create IAM role for EC2, %v", err)
-	// 	return
-	// }
-
 	client := ec2.NewFromConfig(cfg)
 
 	instanceId, err := createInstance(client, "MyEC2InstanceProfile")
@@ -50,20 +42,46 @@ func CreateEC2Instance(name string) {
 		return
 	}
 
-	// sendBashScriptToInstance(cfg, instanceId)
 	fmt.Printf("Intance succesfully created. Instance ID: %v\n", instanceId)
 }
 
 func createInstance(client *ec2.Client, instanceProfileName string) (string, error) {
 
-	bashScript := `#!/bin/bash
-useradd -m -d /home/workspace developer
+	userData := `#!/bin/bash
+useradd -m -d /home/coderino coderino
 
-cd /home/workspace
-wget https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v1.93.0/openvscode-server-v1.93.0-linux-x64.tar.gz
-tar -xzf openvscode-server-v1.93.0-linux-x64.tar.gz
-cd openvscode-server-v1.93.0-linux-x64
-./bin/openvscode-server --host 0.0.0.0`
+curl -fsSL https://get.docker.com | bash
+
+# Modify Docker daemon configuration
+cat > /etc/docker/daemon.json <<EOF
+{
+  "hosts": ["unix:///var/run/docker.sock", "tcp://0.0.0.0:2375"]
+}
+EOF
+
+# Create a systemd drop-in file to modify the Docker service
+mkdir -p /etc/systemd/system/docker.service.d
+cat > /etc/systemd/system/docker.service.d/override.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl start docker
+
+usermod -aG docker coderino
+
+if grep -q sudo /etc/group; then
+	usermod -aG sudo,docker coderino
+elif grep -q wheel /etc/group; then
+	usermod -aG wheel,docker coderino
+fi
+
+echo "coderino ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/91-coderino
+
+`
 
 	runResult, err := client.RunInstances(context.TODO(), &ec2.RunInstancesInput{
 		MinCount:     aws.Int32(1),
@@ -77,7 +95,7 @@ cd openvscode-server-v1.93.0-linux-x64
 		IamInstanceProfile: &types.IamInstanceProfileSpecification{
 			Name: aws.String(instanceProfileName),
 		},
-		UserData: aws.String(base64.StdEncoding.EncodeToString([]byte(bashScript))),
+		UserData: aws.String(base64.StdEncoding.EncodeToString([]byte(userData))),
 	})
 
 	if err != nil {
@@ -94,82 +112,4 @@ func waitForInstanceRunning(client *ec2.Client, instanceId string) error {
 	}, *aws.Duration(time.Hour))
 
 	return err
-}
-
-func sendBashScriptToInstance(cfg aws.Config, instanceID string) error {
-	ssmClient := ssm.NewFromConfig(cfg)
-
-	_, err := ssmClient.SendCommand(context.TODO(), &ssm.SendCommandInput{
-		DocumentName: aws.String("AWS-RunShellScript"),
-		InstanceIds:  []string{instanceID},
-		Parameters: map[string][]string{
-			"commands": {
-				"echo \"Hello from EC2!\" > /home/ec2-user/hello.txt",
-			},
-		},
-	})
-
-	return err
-}
-
-func createIAMRoleForEC2(cfg aws.Config) (string, error) {
-	iamClient := iam.NewFromConfig(cfg)
-
-	// Define the assume role policy document for EC2 to assume the role
-	assumeRolePolicy := `{
-		"Version": "2012-10-17",
-		"Statement": [
-			{
-				"Effect": "Allow",
-				"Principal": {
-					"Service": "ec2.amazonaws.com"
-				},
-				"Action": "sts:AssumeRole"
-			}
-		]
-	}`
-
-	// Create the IAM role
-	roleName := "EC2SSMRole"
-	_, err := iamClient.CreateRole(context.TODO(), &iam.CreateRoleInput{
-		RoleName:                 aws.String(roleName),
-		AssumeRolePolicyDocument: aws.String(assumeRolePolicy),
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	// Attach the AmazonSSMManagedInstanceCore policy to the role
-	_, err = iamClient.AttachRolePolicy(context.TODO(), &iam.AttachRolePolicyInput{
-		RoleName:  aws.String(roleName),
-		PolicyArn: aws.String("arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"),
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	// Create an instance profile for the role
-	instanceProfileName := "EC2SSMInstanceProfile"
-	_, err = iamClient.CreateInstanceProfile(context.TODO(), &iam.CreateInstanceProfileInput{
-		InstanceProfileName: aws.String(instanceProfileName),
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	// Add the role to the instance profile
-	_, err = iamClient.AddRoleToInstanceProfile(context.TODO(), &iam.AddRoleToInstanceProfileInput{
-		InstanceProfileName: aws.String(instanceProfileName),
-		RoleName:            aws.String(roleName),
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	// Return the instance profile name
-	return instanceProfileName, nil
 }
