22145edf2095eaa75e3e00dbc7584113ae0d7f49
Marco Rehmer <mrehmer-services@mailbox.org>
2024-10-02 17:35:18 +0200

    automate workspace creation

--- src/provider/aws/aws-provider.controller.ts
+++ src/provider/aws/aws-provider.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AwsProviderService } from './aws-provider.service';
 
 @Controller('aws')
 export class AwsProviderController {
   constructor(private readonly awsService: AwsProviderService) {}
 
-  @Get('create')
-  createWorkspace() {
-    this.awsService.createWorkspace();
+  @Get('create/:amount')
+  async createWorkspace(@Param() params: any) {
+    const createdIds = await this.awsService.createWorkspace(params.amount);
+    return { created: createdIds };
+  }
+
+  @Get('terminate/all')
+  async terminateAllWorkspaces() {
+    const terminatedIds = await this.awsService.terminateAll();
+    return { terminated: terminatedIds };
+  }
+
+  @Get('terminate/:id')
+  async terminateSingleWorkspace(@Param() params: any) {
+    const terminatedInstanceIds = await this.awsService.terminateInstances([
+      params.id,
+    ]);
+    return { terminated: terminatedInstanceIds };
   }
 }
